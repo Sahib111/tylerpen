22179d13f4576a5f6601abcf08ea4f6f6b22c862
Sahib111 <58338778+Sahib111@users.noreply.github.com>
2021-09-22 09:38:07 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -2,10 +2,11 @@
 //get data from form  
 $name = $_POST['name'];
 $email= $_POST['email'];
+$subject= $_POST['subject'];
 $message= $_POST['message'];
 $to = "ssleonardtyler@gmail.com";
 $subject = "You have a Message!!!";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message . "\r\n  subject = " . $subject;
 $headers = "From: noreply@yoursite.com" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
